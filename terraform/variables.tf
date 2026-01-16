095d3f10f43b7fc847e31377c5e293b14e194205
Audun Nes <aunes@dfds.com>
2026-01-16 11:31:07 +0100

    Fix variable descriptions in Terraform configuration for clarity and consistency

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,38 +1,38 @@
 variable "aws_region" {
   type    = string
-  description = "The AWS region to deploy resources in"
+  description = "The AWS region to deploy resources in."
   default = "eu-west-1"
 }
 
 variable "account_id" {
   type        = string
-  description = "The AWS account ID where resources will be deployed"
+  description = "The AWS account ID where resources will be deployed."
 }
 
 variable "prefix" {
   type        = string
-  description = "Prefix for resource names"
+  description = "Prefix for resource names."
   default     = "k8s-to-aws-examples"
 }
 
 variable "rds_resource_id" {
   type        = string
-  description = "The RDS resource identifier"
+  description = "The RDS resource identifier. Example: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'"
 }
 
 variable "s3_bucket_name" {
   type        = string
-  description = "The name of the S3 bucket to be used"
+  description = "The name of the S3 bucket to be used. Example: 'myapp-bucket'"
 }
 
 variable "ssm_parameters" {
   type = list(string)
-  description = "A list of SSM parameter paths to be read"
+  description = "A list of SSM parameter paths to be read. Example: ['/myapp/db/password', '/myapp/db/username']"
   default = ["*"]
 }
 
 variable "tags" {
   type        = map(string)
-  description = "A map of tags to assign to resources"
+  description = "A map of tags to assign to resources."
   default     = {}
 }
